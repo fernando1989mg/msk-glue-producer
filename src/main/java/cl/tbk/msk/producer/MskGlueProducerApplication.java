0a45a05334e6b1c3af01d14272ba7b50607da027
Fernando Muñoz <fernando1989mg@gmail.com>
2023-07-17 13:00:58 -0400

    healthcheck

--- src/main/java/cl/tbk/msk/producer/MskGlueProducerApplication.java
+++ src/main/java/cl/tbk/msk/producer/MskGlueProducerApplication.java
@@ -2,12 +2,20 @@ package cl.tbk.msk.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MskGlueProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MskGlueProducerApplication.class, args);
 	}
+	
+    @GetMapping("/health")
+    public String getMessages() {
+        return "200!";
+    }
 
 }
